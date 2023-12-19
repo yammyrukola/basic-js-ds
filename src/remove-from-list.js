5e85f4dc6a7c0738bc5a87c19308ceb098c4443a
gitalexus <ru9.alex@gmail.com>
2023-12-19 19:04:27 +0300

    feat: add rest DS

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Given a singly linked list of integers l and an integer k,
@@ -22,9 +22,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *   }
  * }
  */
-function removeKFromList(/* l, k */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+f
+function removeKFromList(l, k) {
+  return l = _del(l, k);
+
+  function _del(l, k) {
+    if (!l) return null;
+    if (l.value === k) {
+      return _del(l.next, k);
+    } else {
+      l.next = _del(l.next, k);
+      return l;
+    }
+  }
 }
 
 module.exports = {
